8d81c8e4dbf913dd515784d03bfbdd22bc7f83a2
Adam Malantonio <adam.malantonio@gmail.com>
2015-01-28 17:10:24 -0500

    updated wc-lists

--- lib/wc-lists.php
+++ lib/wc-lists.php
@@ -1,6 +1,78 @@
 <?php
 use Sunra\PhpSimple\HtmlDomParser;
 
+function get_lists($username, $show_items = false) {
+    $out = array();
+    $body = HtmlDomParser::file_get_html(get_url($username));
+    $lists = $body->find('.table-results-lists tbody tr');
+    $i = 0;
+    foreach($lists as $list) {
+        $name_raw = $list->find('.list a');
+        $name = !empty($name_raw) ? $name_raw[0]->plaintext : "";
+        $id = !empty($name_raw) ? extract_list_id($name_raw[0]->attr['href']) : "";
+        
+        $desc_raw = $list->find('.list .description');
+        $desc = !empty($desc_raw) ? trim($desc_raw[0]->plaintext) : "";
+
+        $out[$i] = array(
+            "name" => $name,
+            "id" => $id,
+            "description" => $desc
+        );
+        
+        if ( $show_items ) {
+            $out[$i]['items'] = !empty($id) ? get_list_items($username, $id) : array();
+        }
+        $i++;
+    }
+
+    return $out;
+}
+
+function get_list_items($username, $listID) {
+    $out = array();
+    $list = HtmlDomParser::file_get_html(get_url($username, $listID));
+    $items = $list->find('.table-results-list tbody tr');
+    
+    foreach($items as $item) {
+        $name = $item->find('.name a')[0];
+        $oclc_no = extract_oclc_number($name->attr['href']);
+        
+        $title = $name->plaintext;
+
+        $author_raw = $item->find('.author');
+        $author = !empty($author_raw) 
+                ? trim(preg_replace("/^\s*by/i", "", $author_raw[0]->plaintext))
+                : "";
+        
+        // I'm having a heck of a time removing the trailing space,
+        // I think it's an encoding issue?
+        $type_raw = $item->find('.type');
+        $format = !empty($type_raw) 
+                ? trim(str_replace('\u00a0', "", (html_entity_decode($type_raw[0]->plaintext))))
+                : "";
+
+        $notes_raw = $item->find('div.description');
+        $notes = !empty($notes_raw)
+               ? trim(preg_replace("/^\s*notes\:/i", "", $notes_raw[0]->plaintext))
+               : ""
+               ;
+        
+        $added = $item->find('.social .date');
+        $date = !empty($added) ? trim(preg_replace("/^\s*added/i", "", $added[0]->plaintext)) : "";
+
+        $out[] = array(
+            "title" => $title,
+            "author" => $author,
+            "format" => $format,
+            "oclc_number" => $oclc_no,
+            "notes" => $notes,
+            "date_added" => $date
+        );
+    }
+    return $out;
+}
+
 // taken from: http://magp.ie/2011/01/06/remove-non-utf8-characters-from-string-with-php/
 function clean_str($str) {
     $str = preg_replace('/[\x00-\x08\x10\x0B\x0C\x0E-\x19\x7F]'.
@@ -36,76 +108,6 @@ function extract_oclc_number($url) {
 
     preg_match($oclc_regex, $url, $m);
     return isset($m[1]) ? $m[1] : null;
-}
-
-function get_lists($username, $show_items = false) {
-    $out = array();
-    $body = HtmlDomParser::file_get_html(get_url($username));
-    $lists = $body->find('.table-results-lists tbody tr');
-    foreach($lists as $list) {
-        $name_raw = $list->find('.list a');
-        $name = !empty($name_raw) ? $name_raw[0]->plaintext : "";
-        $id = !empty($name_raw) ? extract_list_id($name_raw[0]->attr['href']) : "";
-        
-        $desc_raw = $list->find('.list .description');
-        $desc = !empty($desc_raw) ? trim($desc_raw[0]->plaintext) : "";
-
-        $out[] = array(
-            "name" => $name,
-            "id" => $id,
-            "description" => $desc
-        );
-        
-        if ( $show_items ) {
-            $out['items'] = !empty($id) ? get_list_items($username, $id) : array();
-        }
-    }
-
-    return $out;
-}
-
-function get_list_items($username, $listID) {
-    $out = array();
-    $list = HtmlDomParser::file_get_html(get_url($username, $listID));
-    $items = $list->find('.table-results-list tbody tr');
-    
-    foreach($items as $item) {
-        $name = $item->find('.name a')[0];
-        $oclc_no = extract_oclc_number($name->attr['href']);
-        
-        $title = $name->plaintext;
-
-        $author_raw = $item->find('.author');
-        $author = !empty($author_raw) 
-                ? trim(preg_replace("/^\s*by/i", "", $author_raw[0]->plaintext))
-                : "";
-        
-        // I'm having a heck of a time removing the trailing space,
-        // I think it's an encoding issue?
-        $type_raw = $item->find('.type');
-        $format = !empty($type_raw) 
-                ? trim(clean_str(html_entity_decode($type_raw[0]->plaintext)))
-                : "";
-
-        $notes_raw = $item->find('div.description');
-        $notes = !empty($notes_raw)
-               ? trim(preg_replace("/^\s*notes\:/i", "", $notes_raw[0]->plaintext))
-               : ""
-               ;
-        
-        $added = $item->find('.social .date');
-        $date = !empty($added) ? trim(preg_replace("/^\s*added/i", "", $added[0]->plaintext)) : "";
-
-        $out[] = array(
-            "title" => $title,
-            "author" => $author,
-            "format" => $format,
-            "oclc_number" => $oclc_no,
-            "notes" => $notes,
-            "date_added" => $date
-        );
-    }
-    return $out;
 }
 
 function get_url($username, $listID = null) {
